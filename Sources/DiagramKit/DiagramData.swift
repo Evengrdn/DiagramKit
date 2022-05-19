4d03866b4bbf7dcdb5991676aff94fa3b2fc3b4c
Maksim Kuznetsov <metaturned@gmail.com>
2022-05-20 00:01:12 +0500

    Open struct init

--- Sources/DiagramKit/DiagramData.swift
+++ Sources/DiagramKit/DiagramData.swift
@@ -11,6 +11,14 @@ import SwiftUI
 @available(macOS 10.15, *)
 public struct DiagramData: Identifiable, Equatable {
     
+    public init(id: UUID = UUID(), value: Double, title: String, color: Color, format: @escaping (Double) -> String) {
+        self.id = id
+        self.value = value
+        self.title = title
+        self.color = color
+        self.format = format
+    }
+    
     public static func == (lhs: DiagramData, rhs: DiagramData) -> Bool {
         lhs.id == rhs.id
     }
